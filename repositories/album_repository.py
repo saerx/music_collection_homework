801d13b8d23b89f75dadcbf9732643ed99b5c588
saerx <saerlaith.robyn@gmail.com>
2020-11-24 20:29:53 +0000

    added select to album_repository

--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -1,6 +1,7 @@
 from models.artist import Artist
 from models.album import Album
 from db.run_sql import run_sql
+import artist_repository as artist_repository
 
 def save(album):
     sql = "INSERT INTO albums (artist_id, title, genre) VALUES (%s, %s, %s) RETURNING id"
@@ -11,5 +12,23 @@ def save(album):
     return album
 
 #READ
+def select_all():
+    albums = []
+    sql = "SELECT * FROM albums"
+    results = run_sql(sql)
+    for row in results:
+        artist = artist_repository.select(row['artist_id'])
+        album = Album(artist, row['title'], row['genre'], row['id'])
+        albums.append(album)
+    return albums
 
-
+def select(id):
+    album = None
+    sql = "SELECT * FROM albums WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)
+    if result is not None:
+        artist_id = result['artist_id']
+        artist = artist_repository.select(artist_id)
+        album = Album(artist, result['title'], result['genre'])
+    return album
