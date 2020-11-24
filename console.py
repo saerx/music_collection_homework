868bb83fd970574c2960a30e8c21847bad077f22
saerx <saerlaith.robyn@gmail.com>
2020-11-24 20:37:01 +0000

    populated console.py

--- console.py
+++ console.py
@@ -4,3 +4,13 @@ from models.artist import Artist
 import repositories.album_repository as album_repository
 import repositories.artist_repository as artist_repository
 
+album_repository.delete_all()
+artist_repository.delete_all()
+
+CSS = Artist("CSS")
+artist_repository.save(CSS)
+
+eponymous_css = Album(CSS, "Cansei de Ser Sexy", "electroclash")
+album_repository.save(eponymous_css)
+
+pdb.set_trace()
